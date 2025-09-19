0c9f498b081a471d0fcf9e66afff447b787f0165
Prince Mishra <prince.mishra@outlook.in>
2025-09-20 01:05:11 +0530

    Update app.py
    Fixed path issue

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template
 import numpy as np
 import pickle
 app = Flask(__name__)
-model = pickle.load(open('C:\\Users\\princ\\Downloads\\Programs\\Crop Recommendation System\\model.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -15,4 +15,5 @@ def predict():
     output = prediction[0]
     return render_template('index.html', prediction_text='Recommended Crop is: {}'.format(output))
 if __name__ == "__main__":
+
     app.run(debug=True)
