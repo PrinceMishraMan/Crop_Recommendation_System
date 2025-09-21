c30af7e78390278da331380b9bcd73029913a682
Prince Mishra <prince.mishra@outlook.in>
2025-09-22 02:05:00 +0530

    Update app.py
    Fixed errors

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, render_template
 import numpy as np
 import pickle
+import joblib
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
+sc = joblib.load(open('scaler.joblib', 'rb'))
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -33,5 +35,6 @@ def login():
 if __name__ == "__main__":
 
     app.run(debug=True)
+
 
 
